ca2b234ba761778548ecbdb9a98be4f8381bc694
orionheidi <mikimojamacka1983>
2019-03-19 18:32:23 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/cars">Cars </router-link>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import Cars from './components/Cars.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cars
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Cars from './components/Cars.vue'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
+Vue.use(VueRouter) 
 Vue.config.productionTip = false
 
+
+const routes = [
+  { path: '/cars', component:Cars,props:true},
+  
+]
+
+const router = new VueRouter({
+    routes:routes,
+    mode: 'history',
+    //moze i samo routes
+})
+
 new Vue({
+  router:router,
   render: h => h(App),
 }).$mount('#app')
